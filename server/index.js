c7aa6dba2a7fcffc6c5015d198aea83a5394d432
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2024-01-20 00:37:38 +0530

    User creation and room joining implemented

--- server/index.js
+++ server/index.js
@@ -1,63 +1,44 @@
-const express=require('express');
-const app=express();
-const httpServer=require('http').createServer(app);
-const {Server}=require('socket.io');
-const cors=require("cors");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const server = require("http").createServer(app);
+const {Server} = require("socket.io");
+
+const io =new Server(server);
 
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
+app.get("/", (req, res) => {
+  res.send("server");
+});
 
-const io= new Server(httpServer,{
-    cors:{
-        origin:"http://localhost:5173",
-        methods:["GET","POST"],
-    },
-})
-
-let connections=[];
+// socket.io
 
 io.on('connection',(socket)=>{
-    connections.push(socket);
-  
-      console.log(`${socket.id} is connected`);
-
-      socket.on("join_room", (data)=>{
-        socket.join(data);
-      })
-
-      socket.on("send_message" , (data)=>{
-        socket.to(data.room).emit("receive_message", data)
-      })
-
-      socket.on('draw',(data)=>{
-        connections.forEach(con =>{
-            if(con.id!=socket.id){
-                con.emit('ondraw',{x:data.x,y:data.y});
-            }
-        })
-    })
-    socket.on('down',(data)=>{
-        connections.forEach(con =>{
-            if(con.id!=socket.id){
-                con.emit('ondown',{x:data.x,y:data.y});
-            }
-        })
-    })
-
-      socket.on('disconnect', ()=>{
-        console.log(`${socket.id} disconnected`);
-        connections=connections.filter((con)=>{con.id!==socket.id})
-        
-      })
-     
-})
-
-
-
-httpServer.listen(8000,()=>{
-    console.log("server listening on port 8000");
+  socket.on("userJoined", (data)=>{
+    const {name, userId, roomId, host, presenter} = data;
+    socket.join(roomId);
+    socket.emit("userIsJoined", {success:true});
+  })
   
 })
+
+
+// serve on port
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () =>
+  console.log(`server is listening on http://localhost:${PORT}`)
+);
 
 
 
--- white-board/src/components/Forms/JoinRoomForm/Joinroom.tsx
+++ white-board/src/components/Forms/JoinRoomForm/Joinroom.tsx
@@ -1,13 +1,42 @@
-
+import { Socket } from 'socket.io-client';
 import './joinroom.css'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
-const Joinroom = () => {
+interface Props{
+  socket:Socket
+  uuid:()=>string
+  setUser: (user: { name: string; roomId: string; userid: string; host: boolean; presenter: boolean }) => void;
+}
+
+const Joinroom = ({socket,setUser,uuid}:Props) => {
+
+const [roomId,setRoomId]=useState("");
+const [name,setName]=useState("");
+const navigate = useNavigate();
+
+const handleRoomJoin=(e:React.FormEvent)=>{
+  e.preventDefault();
+
+  const roomData={
+    name,
+    roomId,
+    userid:uuid(),
+    host:true,
+    presenter:true
+   };
+   setUser(roomData);
+   navigate(`/${roomId}`)
+   socket.emit("userJoined",roomData);
+}
+
   return (
     <form>
     <div>
         <input
  type="text"
  placeholder="Enter your name"
+ onChange={(e)=>setName(e.target.value)}
         />
     </div>
     <div>
@@ -15,11 +44,12 @@ const Joinroom = () => {
             <input
             type="text"
             placeholder="Generate room code"
+            onChange={(e)=>setRoomId(e.target.value)}
             />
      
         </div>
         </div>
-        <button type="submit">Generate Room</button>
+        <button type="submit" onClick={handleRoomJoin}>Generate Room</button>
 </form>
   )
 }
