2a42c9d4e5c4c75fe78b56d9ed67c56a70100239
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2024-03-18 00:43:11 +0530

    Jwt token added and protected routes created

--- server/db/db.js
+++ server/db/db.js
@@ -3,7 +3,7 @@ const mongoose=require("mongoose")
 const Connection = async(USERNAME,PASSWORD)=>{
     const mongoURL=`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.krcqboi.mongodb.net/?retryWrites=true&w=majority`
      try{
-          await mongoose.connect(mongoURL,{ useUnifiedTopology: true, useNewUrlParser: true});
+          await mongoose.connect(mongoURL);
           console.log("Database connected successfully");
      }catch(error){
         console.log('Error', error.message);
@@ -11,3 +11,4 @@ const Connection = async(USERNAME,PASSWORD)=>{
 }
 
 module.exports= Connection
+
