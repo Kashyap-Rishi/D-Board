1cf62d0afe5a0d89c8d5a00ce7c25a70ecfa3127
lOstinsOul <99341814+lOstinsOul@users.noreply.github.com>
2024-03-27 11:26:58 +0530

    Fixes

--- server/routes/data/fileData.js
+++ server/routes/data/fileData.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Canvas = require("../../models/Canvas");
+const Canvas = require("../../models/canvas");
 
 router.post("/files", async (req, res) => {
   try {
